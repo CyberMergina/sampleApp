9c3bd5a6a1962cf1236160395ad1c3bfa09d117a
CyberMergina <cyber.mergina@gmail.com>
2017-10-30 23:36:14 +0900

    show web page in webView.

--- sampleApp/ViewController.swift
+++ sampleApp/ViewController.swift
@@ -9,22 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var stopButtom: UIBarButtonItem!
+    @IBOutlet weak var stopButton: UIBarButtonItem!
     @IBOutlet weak var reloadButton: UIBarButtonItem!
     @IBOutlet weak var backButton: UIBarButtonItem!
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var webView: UIWebView!
+    
+    // デフォルトで開きたいページ
+    let homeUrlString = "http://www.yahoo.co.jp"
 
+    // コントローラーが持つビューが読み込まれた後に実行される
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // ホームページを開く
+        open(urlString: homeUrlString)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // 文字列で指定されたURLをWebViewで開く
+    func open(urlString: String) {
+        let url = URL(string: urlString)
+        let urlRequest = URLRequest(url: url!)
+        webView.loadRequest(urlRequest)
+    }
 
+    @IBAction func backButtonTapped(_ sender: UIBarButtonItem) {
+    }
 
+    @IBAction func reloadButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: UIBarButtonItem) {
+    }
 }
 
